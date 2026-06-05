2ef9c22d9ed2363605e1cac47397d8291429834b
Alexander Mills <alexander.d.mills@gmail.com>
2026-06-05 14:03:21 -0500

    Manage Airbyte S3 credentials with Terraform

--- remote/terraform/aws/airbyte-s3/outputs.tf
+++ remote/terraform/aws/airbyte-s3/outputs.tf
@@ -12,3 +12,13 @@ output "bucket_region" {
   description = "AWS region where the Airbyte S3 bucket is managed."
   value       = var.aws_region
 }
+
+output "iam_user_arn" {
+  description = "IAM user ARN for Airbyte S3 access."
+  value       = aws_iam_user.airbyte.arn
+}
+
+output "secrets_manager_secret_name" {
+  description = "Secrets Manager secret containing Airbyte S3 credentials."
+  value       = aws_secretsmanager_secret.airbyte_s3.name
+}
