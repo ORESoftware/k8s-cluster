2ecf8e429db31c498e08f0028bd411f65cce40bd
alex-mills <alexander.d.mills@gmail.com>
2026-06-26 22:55:14 -0500

    Add Nix development shell

---
+++ nix/dev-shell.nix
@@ -0,0 +1,39 @@
+{ pkgs }:
+let
+  shellPackages = with pkgs; [
+    argocd
+    awscli2
+    bacon
+    cargo
+    clippy
+    curl
+    git
+    go
+    jq
+    just
+    kubectl
+    kustomize
+    kubernetes-helm
+    nodejs_22
+    opentofu
+    pnpm_10
+    postgresql_16
+    rust-analyzer
+    rustc
+    rustfmt
+    shellcheck
+    shfmt
+    yq-go
+  ];
+in
+pkgs.mkShell {
+  packages = shellPackages;
+
+  LANG = if pkgs.stdenv.hostPlatform.isDarwin then "en_US.UTF-8" else "C.UTF-8";
+  LC_ALL = if pkgs.stdenv.hostPlatform.isDarwin then "en_US.UTF-8" else "C.UTF-8";
+
+  shellHook = ''
+    export AWS_PROFILE="''${AWS_PROFILE:-dd-codex}"
+    export NIX_DEV_SHELL=dd-k8s-cluster
+  '';
+}
