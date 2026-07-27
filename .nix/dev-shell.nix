56f0b3e6cd3566d2faf5a8d45dba62dfd652fc19
Alexander Mills <alex@oresoftware.com>
2026-07-27 14:40:34 -0500

    DEN-330 apply canonical nixfmt formatting to the dev shell

--- .nix/dev-shell.nix
+++ .nix/dev-shell.nix
@@ -1,35 +1,37 @@
 { pkgs, agentCheck }:
 let
-  shellPackages = (with pkgs; [
-    actionlint
-    argocd
-    awscli2
-    bacon
-    cargo
-    clippy
-    curl
-    dart
-    beamPackages.erlang
-    git
-    gleam
-    go
-    jq
-    just
-    kubectl
-    kustomize
-    kubernetes-helm
-    nixfmt-rfc-style
-    nodejs_22
-    opentofu
-    pnpm_10
-    postgresql_16
-    rust-analyzer
-    rustc
-    rustfmt
-    shellcheck
-    shfmt
-    yq-go
-  ]) ++ [ agentCheck ];
+  shellPackages =
+    (with pkgs; [
+      actionlint
+      argocd
+      awscli2
+      bacon
+      cargo
+      clippy
+      curl
+      dart
+      beamPackages.erlang
+      git
+      gleam
+      go
+      jq
+      just
+      kubectl
+      kustomize
+      kubernetes-helm
+      nixfmt-rfc-style
+      nodejs_22
+      opentofu
+      pnpm_10
+      postgresql_16
+      rust-analyzer
+      rustc
+      rustfmt
+      shellcheck
+      shfmt
+      yq-go
+    ])
+    ++ [ agentCheck ];
 in
 pkgs.mkShell {
   packages = shellPackages;
