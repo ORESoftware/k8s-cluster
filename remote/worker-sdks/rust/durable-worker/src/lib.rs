38368302f07d0b94eeba320c3cc46f17bfb37f13
Alexander Mills <alex@oresoftware.com>
2026-08-05 14:38:44 -0500

    feat(workers): expose Rust SDK client and worker API

---
+++ remote/worker-sdks/rust/durable-worker/src/lib.rs
@@ -0,0 +1,19 @@
+#![forbid(unsafe_code)]
+
+mod client;
+mod error;
+mod transport;
+mod worker;
+
+pub use client::{
+    Assignment, Client, ClientOptions, JsonObject, Lease, StepCompletion, StepFailure,
+    StepOutput, WorkerPoll, WorkerRegistration,
+};
+pub use error::{DurableWorkerError, ProtocolError, TransportError};
+pub use transport::{
+    ReqwestTransport, Transport, TransportFuture, TransportRequest, TransportResponse,
+};
+pub use worker::{
+    Cancellation, Handler, HandlerFuture, TaskContext, Worker, WorkerApi, WorkerConfig,
+    WorkerFailure, WorkerFuture, WorkerSummary,
+};
